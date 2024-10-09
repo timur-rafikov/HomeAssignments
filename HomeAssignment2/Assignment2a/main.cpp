bc9c66bd9c25c843d8a48e674ef5f40aa64d3958
Timur Rafikov <st128186@student.spbu.ru>
2024-10-09 12:03:39 +0300

    I found out the file size, declared an array and wrote the file data there, then output it to the console. Added a Makefile

--- HomeAssignment2/Assignment2a/main.cpp
+++ HomeAssignment2/Assignment2a/main.cpp
@@ -1,14 +1,35 @@
 #include <fstream>
+#include <filesystem>
 #include <iostream>
 
 int main() {
-	std::ifstream infile("source.txt", std::ios::binary);
-	//infile.open("source.txt", std::ios::binary | std::ios::in);
-
-	std::string s;
-	while (infile >> s)
-		std::cout << s << ' ';
+	// Reading the binary file
+	std::ifstream infile("source", std::ios::binary);
+	
+	// Output (optional)
+	/*
+	char c;
+	while (infile >> c) {
+		std::cout << (int)c << '\n';
+	}
 	std::cout << '\n';
+	*/
+
+	// Saving the file size
+	std::size_t fsize = std::filesystem::file_size("source");
+	std::cout << "Size of binary file: " << fsize << "\n\n";
+
+	// Declaring an array and writing
+	int *arr = new int[fsize];
+	char c;
+	for (char i = 0, c = 0; infile >> c; i++) {
+		arr[i] = (int)c;
+	}
+
+	// Check array in output
+	for (int i = 0; i < fsize; ++i) {
+		std::cout << arr[i] << '\n';
+	}
 
 	return 0;
 }
